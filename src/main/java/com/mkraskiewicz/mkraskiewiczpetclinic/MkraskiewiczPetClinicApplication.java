5b160cf3aaae9a715407062aa367b6fa64869012
Maciej Kraskiewicz <maciek.kraskiewicz@gmail.com>
2019-01-15 21:40:38 +0100

    Initial commit

---
+++ src/main/java/com/mkraskiewicz/mkraskiewiczpetclinic/MkraskiewiczPetClinicApplication.java
@@ -0,0 +1,14 @@
+package com.mkraskiewicz.mkraskiewiczpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MkraskiewiczPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MkraskiewiczPetClinicApplication.class, args);
+    }
+
+}
+
