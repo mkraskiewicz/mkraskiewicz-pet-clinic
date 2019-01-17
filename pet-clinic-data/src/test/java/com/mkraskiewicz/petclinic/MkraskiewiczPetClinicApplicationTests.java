5edd57f30d073c57df2bfba6f7ae4be9db0e0263
mkraskiewicz <maciek.kraskiewicz@gmail.com>
2019-01-17 13:27:44 +0100

    Empty change

--- pet-clinic-data/src/test/java/com/mkraskiewicz/petclinic/MkraskiewiczPetClinicApplicationTests.java
+++ pet-clinic-data/src/test/java/com/mkraskiewicz/petclinic/MkraskiewiczPetClinicApplicationTests.java
@@ -10,7 +10,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class MkraskiewiczPetClinicApplicationTests {
 
     @Test
-    public void contextLoads() {
+    public void contextLoadsTest() {
     }
 
 }
